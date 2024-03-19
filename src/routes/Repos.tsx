c154f5855385dfcbcc84a9e74786d0ffd0f45d3a
Adriano Pessoa <adrianobpessoa@gmail.com>
2024-03-19 16:48:36 -0300

    feat: create request and print repos name

--- src/routes/Repos.tsx
+++ src/routes/Repos.tsx
@@ -1,14 +1,51 @@
 import { useParams } from "react-router-dom"
 import classes from "./Repos.modules.css"
 import BackBtn from "../components/BackBtn";
+import { useEffect, useState } from "react";
+import { RepoProps } from "./Repo";
+import Loader from "../components/Loader";
 
 const Repos = () => {
 
     const {username} = useParams();
+    const [repos, setRepos] = useState<RepoProps[] | [] | null>(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+      
+      const loadRepos = async (username: string) => {
+        setIsLoading(true);
+        const res =await fetch(`https://api.github.com/users/${username}/repos`);
+        
+        const data = await res.json();
+
+        setRepos(data);
+
+        setIsLoading(false);
+
+      };
+
+      if(username){
+        loadRepos(username);
+      };
+    }, []);
+
+    if(!repos && isLoading) {
+      return <Loader/>
+    }
+
   return (
     <div>
-        <h2>Repos {username}</h2>
         <BackBtn/>
+        <h2>Explore os repositórios do usuário: {username}</h2>
+        {repos && repos.length === 0 && <p>Não há repositórios!</p>}
+        {repos && repos.length > 0 && (
+          <div>
+            {repos.map((repo: RepoProps) => (
+              <p key={repo.name}>{repo.name}</p>
+            ))}
+          </div>
+        )}
     </div>
   )
 }
